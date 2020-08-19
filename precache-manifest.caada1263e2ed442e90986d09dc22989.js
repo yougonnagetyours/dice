83b5a27c81d9ac5ac6372f1e18b7427f17933633
Michal Potoczny <michal.potoczny.92@gmail.com>
2020-08-19 20:06:05 +0200

    Updates

---
+++ precache-manifest.caada1263e2ed442e90986d09dc22989.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a964970cb06a9cb07e4",
+    "url": "/dice/static/css/main.ffec22fe.chunk.css"
+  },
+  {
+    "revision": "4a964970cb06a9cb07e4",
+    "url": "/dice/static/js/main.e30a261b.chunk.js"
+  },
+  {
+    "revision": "31385d9ad14271274f77",
+    "url": "/dice/static/js/runtime~main.fe841fa0.js"
+  },
+  {
+    "revision": "f6d2ab47bb337dc705a8",
+    "url": "/dice/static/js/2.b67fbdca.chunk.js"
+  },
+  {
+    "revision": "649ce836c1917f547feb4e0cc3105c20",
+    "url": "/dice/index.html"
+  }
+];
